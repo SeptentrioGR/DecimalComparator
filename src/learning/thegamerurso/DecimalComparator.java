821883cc6a844df17362b5ef8ef99857ecaaa964
Petros Kitsaras <petros.kitsaras@gmail.com>
2021-01-06 15:31:04 +0200

    init commit

---
+++ src/learning/thegamerurso/DecimalComparator.java
@@ -0,0 +1,26 @@
+package learning.thegamerurso;
+//Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+//The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise, return false.
+//
+//EXAMPLES OF INPUT/OUTPUT:
+//* areEqualByThreeDecimalPlaces(-3.1756, -3.175); → should return true since numbers are equal up to 3 decimal places.
+//* areEqualByThreeDecimalPlaces(3.175, 3.176); → should return false since numbers are not equal up to 3 decimal places
+//* areEqualByThreeDecimalPlaces(3.0, 3.0); → should return true since numbers are equal up to 3 decimal places.
+//* areEqualByThreeDecimalPlaces(-3.123, 3.123); → should return false since numbers are not equal up to 3 decimal places.
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b){
+        a = a * 1000;
+        b = b * 1000;
+
+        int a1 = (int) a;
+        int b1 = (int) b;
+
+        if(a1 == b1){
+            System.out.println(true);
+            return  true;
+        }else{
+            System.out.println(false);
+            return  false;
+        }
+    }
+}
